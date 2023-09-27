12ac3f9f270ccd97fed5e1f28bc45072668d980a
clmoro <angy98333@gmail.com>
2023-09-27 12:11:14 +0200

    pub peerings fixed

--- src/custom_msg_pkg/src/custom_loop_closure (copy).cpp
+++ src/custom_msg_pkg/src/custom_loop_closure (copy).cpp
@@ -51,28 +51,32 @@ class LoopClosurePublisher : public rclcpp::Node
 
 // SUBSCRIPTIONS
 
-      // Subscriptions to Custom Odometry
+      // Subscription 1
       subscription1_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r0/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic1_callback, this, _1));
+
+      // Subscription 2
       subscription2_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r1/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic2_callback, this, _1));
+
+      // Subscription 3
       subscription3_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r2/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic3_callback, this, _1));
+
+      // Subscription 4
       subscription4_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r3/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic4_callback, this, _1));
+
+      // Subscription 5
       subscription5_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r4/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic5_callback, this, _1));
+
+      // Subscription 6
       subscription6_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r5/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic6_callback, this, _1));
+
+      // Subscription 7
       subscription7_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r6/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic7_callback, this, _1));
+
+      // Subscription 8
       subscription8_ = this->create_subscription<cslam_common_interfaces::msg::KeyframeOdom>("/r7/cslam/keyframe_odom", 100, std::bind(&LoopClosurePublisher::topic8_callback, this, _1));
 
-      // Subscriptions to Adjacency Matrix
-      sub_peers_1_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_1", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-      sub_peers_2_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_2", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-      sub_peers_3_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_3", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-      sub_peers_4_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_4", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-      sub_peers_5_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_5", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-      sub_peers_6_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_6", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-      sub_peers_7_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_7", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-      sub_peers_8_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/peering_8", 10, std::bind(&LoopClosurePublisher::sub_peers_callback, this, _1));
-
       // Subscription to the Candidate topic
-      subscription_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/candidate_information", 10, std::bind(&LoopClosurePublisher::topic_candidate_callback, this, _1));
+      subscription_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/candidate_information", 10, std::bind(&LoopClosurePublisher::topic_callback, this, _1));
 
       // Subscription to the Blockchain topic
       subscription_blockchain_ = this->create_subscription<std_msgs::msg::Int64MultiArray>("/blockchain_approved_transformation", 10, std::bind(&LoopClosurePublisher::topic_blockchain_callback, this, _1));
@@ -150,61 +154,62 @@ class LoopClosurePublisher : public rclcpp::Node
     }
 
   private:
-    void topic_candidate_callback(const std_msgs::msg::Int64MultiArray::SharedPtr msg) const
-    {
-      auto message_cand = std_msgs::msg::Float64MultiArray();
-
-      // i is the INDEX in the database, [i][0] is the ROBOT_ID, [i][1] is the SCENE_ID, [i][2] is the ODOMETRY X, [i][3] is the ODOMETRY Y, [i][4] is the KEYFRAME_ID
-      candidate[i][0] = msg->data[0];
-      candidate[i][1] = msg->data[1];
-      candidate[i][2] = odom_vector[candidate[i][0]-1].odom.pose.pose.position.x;
-      candidate[i][3] = odom_vector[candidate[i][0]-1].odom.pose.pose.position.y;
-      candidate[i][4] = odom_vector[candidate[i][0]-1].id;
-      RCLCPP_INFO_STREAM(this->get_logger(), "New Candidate descriptor. Updated database: " <<  i + 1 << " - [" << candidate[i][0] << "][" << candidate[i][1] <<"]" << "[" << candidate[i][2] << "][" << candidate[i][3] << "][" << candidate[i][4] <<"]");
-
-      // The message_cand is a vector of what I want to put on the blockchain: [candidate_id, candidate[i][0], candidate[i][1], candidate[i][2], candidate[i][3], candidate[i][4]]
-      message_cand.data = {i + 1, candidate[i][0],candidate[i][1],candidate[i][2],candidate[i][3],candidate[i][4]};
-      publisher_cand_-> publish(message_cand);
-
-      i++;
-    }
-
-  private:
-    void sub_peers_callback(const std_msgs::msg::Int64MultiArray::SharedPtr msg) const
+    void topic_callback(const std_msgs::msg::Int64MultiArray::SharedPtr msg) const
     {
       auto message_transf = std_msgs::msg::Float64MultiArray();
+      auto message_cand = std_msgs::msg::Float64MultiArray();
+      
+      for (int j = 0; j < i + 1; j++) {
 
-      float dx = 0.0;
-      float dy = 0.0;
+        float dx = 0.0;
+        float dy = 0.0;
 
-      // The vector that defines which robot is Byzantine, adding random noise to the generation of its transformation
-      //int byzantine_vector[] = {0, 0, 0, 0, 0, 0, 0, 0};
+        // The vector that defines which robot is Byzantine, adding random noise to the generation of its transformation
+        int byzantine_vector[] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-      if (candidate[j][1] == msg->data[1] && candidate[j][0] != msg->data[0] && candidate[j][0] != 0) {
-      //int random_noise_point_x = -9 + (rand() % 19);
-      //int random_noise_point_y = -9 + (rand() % 19);
+        if (candidate[j][1] == msg->data[1] && candidate[j][0] != msg->data[0] && candidate[j][0] != 0) {
+          int random_noise_point_x = -9 + (rand() % 19);
+          int random_noise_point_y = -9 + (rand() % 19);
              
-      dx = candidate[j][2] - odom_vector[msg->data[0]-1].odom.pose.pose.position.x; //+ byzantine_vector[msg->data[0]-1] * random_noise_point_x;
-      dy = candidate[j][3] - odom_vector[msg->data[0]-1].odom.pose.pose.position.y; //+ byzantine_vector[msg->data[0]-1] * random_noise_point_y;
+          dx = candidate[j][2] - odom_vector[msg->data[0]-1].odom.pose.pose.position.x; //+ byzantine_vector[msg->data[0]-1] * random_noise_point_x;
+          dy = candidate[j][3] - odom_vector[msg->data[0]-1].odom.pose.pose.position.y; //+ byzantine_vector[msg->data[0]-1] * random_noise_point_y;
 
-      RCLCPP_INFO_STREAM(this->get_logger(), "New Loop Closure from robot " << msg->data[0] << " on robot " << candidate[j][0] << " at scene " << msg->data[1]);
-      // RCLCPP_INFO_STREAM(this->get_logger(), message.transform.translation.x << " " << message.transform.translation.y << " " << message.robot0_keyframe_id << " " << message.robot0_id << " " << message.robot1_keyframe_id << " " << message.robot1_id);
+          RCLCPP_INFO_STREAM(this->get_logger(), "New Loop Closure from robot " << msg->data[0] << " on robot " << candidate[j][0] << " at scene " << msg->data[1]);
+          // RCLCPP_INFO_STREAM(this->get_logger(), message.transform.translation.x << " " << message.transform.translation.y << " " << message.robot0_keyframe_id << " " << message.robot0_id << " " << message.robot1_keyframe_id << " " << message.robot1_id);
 
-      // The message_transf is a vector of what I want to put on the blockchain: [id_LC, keyframe0, from: msg->data[0] (remember to do -1), keyframe1, to: candidate[j][0] (remember to do -1), dx, dy]
-      message_transf.data = {id_loop_closure, odom_vector[msg->data[0]-1].id, msg->data[0], candidate[j][4], candidate[j][0], dx, dy};
-      publisher_transf_-> publish(message_transf);
+          // The message_transf is a vector of what I want to put on the blockchain: [id_LC, keyframe0, from: msg->data[0] (remember to do -1), keyframe1, to: candidate[j][0] (remember to do -1), dx, dy]
+          message_transf.data = {id_loop_closure, odom_vector[msg->data[0]-1].id, msg->data[0], candidate[j][4], candidate[j][0], dx, dy};
+          publisher_transf_-> publish(message_transf);
 
-      loop_closure[id_loop_closure][0] = id_loop_closure;
-      loop_closure[id_loop_closure][1] = odom_vector[msg->data[0]-1].id;
-      loop_closure[id_loop_closure][2] = msg->data[0];
-      loop_closure[id_loop_closure][3] = candidate[j][4];
-      loop_closure[id_loop_closure][4] = candidate[j][0];
-      loop_closure[id_loop_closure][5] = dx;
-      loop_closure[id_loop_closure][6] = dy;
+          loop_closure[id_loop_closure][0] = id_loop_closure;
+          loop_closure[id_loop_closure][1] = odom_vector[msg->data[0]-1].id;
+          loop_closure[id_loop_closure][2] = msg->data[0];
+          loop_closure[id_loop_closure][3] = candidate[j][4];
+          loop_closure[id_loop_closure][4] = candidate[j][0];
+          loop_closure[id_loop_closure][5] = dx;
+          loop_closure[id_loop_closure][6] = dy;
 
-      id_loop_closure++;
+          id_loop_closure++;
+
+        }
+        if (j == i) {
+          // i is the INDEX in the database, [i][0] is the ROBOT_ID, [i][1] is the SCENE_ID, [i][2] is the ODOMETRY X, [i][3] is the ODOMETRY Y, [i][4] is the KEYFRAME_ID
+          candidate[i][0] = msg->data[0];
+          candidate[i][1] = msg->data[1];
+          candidate[i][2] = odom_vector[candidate[i][0]-1].odom.pose.pose.position.x;
+          candidate[i][3] = odom_vector[candidate[i][0]-1].odom.pose.pose.position.y;
+          candidate[i][4] = odom_vector[candidate[i][0]-1].id;
+          RCLCPP_INFO_STREAM(this->get_logger(), "New Candidate descriptor. Updated database: " <<  i + 1 << " - [" << candidate[i][0] << "][" << candidate[i][1] <<"]" << "[" << candidate[i][2] << "][" << candidate[i][3] << "][" << candidate[i][4] <<"]");
+
+          // The message_cand is a vector of what I want to put on the blockchain: [candidate_id, candidate[i][0], candidate[i][1], candidate[i][2], candidate[i][3], candidate[i][4]]
+          message_cand.data = {i + 1, candidate[i][0],candidate[i][1],candidate[i][2],candidate[i][3],candidate[i][4]};
+          publisher_cand_-> publish(message_cand);
+
+        }
 
       }
+
+      i++;
     }
 
   private:
@@ -237,14 +242,6 @@ class LoopClosurePublisher : public rclcpp::Node
     rclcpp::Subscription<cslam_common_interfaces::msg::KeyframeOdom>::SharedPtr subscription6_;
     rclcpp::Subscription<cslam_common_interfaces::msg::KeyframeOdom>::SharedPtr subscription7_;
     rclcpp::Subscription<cslam_common_interfaces::msg::KeyframeOdom>::SharedPtr subscription8_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_1_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_2_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_3_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_4_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_5_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_6_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_7_;
-    rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr sub_peers_8_;
     rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr subscription_;
     rclcpp::Subscription<std_msgs::msg::Int64MultiArray>::SharedPtr subscription_blockchain_;
     
